b4cc874e3242fbaaf5cca71e81fce8351195caa6
sumit007ind <sharmaraj81808@gmail.com>
2025-06-26 22:51:22 +0530

    Update Login.jsx

--- Emp-mangement/src/Components/Auth/Login.jsx
+++ Emp-mangement/src/Components/Auth/Login.jsx
@@ -6,10 +6,10 @@ const Login = () => {
         <div className='border-2 border-red-400
         '>
             <form className='flex flex-col items-center justify-center'>
-                <input type='email' placeholder='Enter Your Name'></input>
-                <input type='password' placeholder='Enter Your Password'>
-
-                </input>
+                <input className='text-black outline-none bg-transparent border-2 border-emerald-500  text-xl py -4 px-5 rounded-full placeholder:text-black' type='email' placeholder='Enter Your Name'></input>
+                <input className='text-black outline-none bg-transparent border-2 border-emerald-500  text-xl py -4 px-5 rounded-full mt-4 placeholder:text-black' type='password' placeholder='Enter Your Password'>
+ </input>
+                <button className='text-black outline-none border-2 bg-emerald-500  text-xl py -4 px-5 rounded-full mt-6 mb-2 placeholder:text-black'>Login</button>
             </form>
         </div>
       
