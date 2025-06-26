99ba6852aaaaf39b2c7ba8ef5b638349bd1546bc
sumit007ind <sharmaraj81808@gmail.com>
2025-06-26 22:58:28 +0530

    ///

--- Emp-mangement/src/Components/Auth/Login.jsx
+++ Emp-mangement/src/Components/Auth/Login.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='flex  h-screen w-screen items-center justify-center'>
-        <div className='border-2 border-red-400
+        <div className='border-2 border-emerald-400 p-20 rounded-xl
         '>
             <form className='flex flex-col items-center justify-center'>
-                <input className='text-black outline-none bg-transparent border-2 border-emerald-500  text-xl py -4 px-5 rounded-full placeholder:text-black' type='email' placeholder='Enter Your Name'></input>
-                <input className='text-black outline-none bg-transparent border-2 border-emerald-500  text-xl py -4 px-5 rounded-full mt-4 placeholder:text-black' type='password' placeholder='Enter Your Password'>
+                <input required className='text-black outline-none bg-transparent border-2 border-emerald-500  text-xl py -4 px-5 rounded-full placeholder:text-black' type='email' placeholder='Enter Your Name'></input>
+                <input required className='text-black outline-none bg-transparent border-2 border-emerald-500  text-xl py -4 px-5 rounded-full mt-4 placeholder:text-black' type='password' placeholder='Enter Your Password'>
  </input>
-                <button className='text-black outline-none border-2 bg-emerald-500  text-xl py -4 px-5 rounded-full mt-6 mb-2 placeholder:text-black'>Login</button>
+                <button className='text-black outline-none border-none bg-emerald-500  text-xl py -4 px-5 rounded-full mt-6 mb-2 placeholder:text-black'>Login</button>
             </form>
         </div>
       
