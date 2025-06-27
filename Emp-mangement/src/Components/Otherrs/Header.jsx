2de2519ccb2ea05b4304891fa4195f869cc76cd0
sumit007ind <sharmaraj81808@gmail.com>
2025-06-28 00:43:05 +0530

    new edits

--- Emp-mangement/src/Components/Otherrs/Header.jsx
+++ Emp-mangement/src/Components/Otherrs/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex items-end justify-between '>
-      <h1 className='text-black text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>Sumit</span></h1>
+      <h1 className='text-black text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>Sumit🙌</span></h1>
       <button className='text-white text-xl bg-red-600 px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
